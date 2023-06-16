d87ab208bf0bf64dcda00028205ff11cade58632
zil-web <126854462+zil-web@users.noreply.github.com>
2023-06-16 16:08:03 +0700

    finish

--- stackusingarray/stackusingarray.cpp
+++ stackusingarray/stackusingarray.cpp
@@ -1,97 +1,105 @@
 #include <iostream>
-#include<string>
+#include <string>
 
 using namespace std;
- 
-class stackarray {
+
+class stackArray {
 private:
 	int stack_array[5];
 	int top;
+
 public:
 	//constructor
-	stackarray() {
+	stackArray() {
 		top = -1;
-
 	}
+
 	void push() {
 		int element;
-		cout << "enter an element :";
-			cin >> element;
-		if (top == 4) {
-			cout << "number of data execd the limit" << endl;
-			return ;
-		}
-		top++; // step2
-		stack_array[top] = element; //step3
-		cout << endl;
-		cout << element << "ditambahkan(pushed)" << endl;
+		cout << "Enter element : ";
+		cin >> element;
 
+		if (top == 4) {
+			cout << "\nNumber of data exceed the limit" << endl; //step 1
+			return;
+		}
+		top++; //step 2
+		stack_array[top] = element; //step 3
+		cout << endl;
+		cout << element << " ditambahkan(pushed)" << endl;
 	}
+
 	void pop() {
 		if (empty()) {
-			cout << "\nstack in empty. cannot pop." << endl; //1.a
+			cout << "\nStack is empty. Cannot pop." << endl; //1.a
 			return; //1.b
 		}
-		cout << "\nthe pop element is :" << stack_array[top] << endl; //step 2
-		top--; //1.b
+
+		cout << "\nThe popped element is : " << stack_array[top] << endl; //step 2
+		top--; //step 3
 	}
 
 	//method for check if data is empty
 	bool empty() {
-		return(top == -1);
+		return (top == -1);
 	}
+
 	void display() {
 		if (empty()) {
-			cout << "\nstack is empty." << endl;
+			cout << "\nStack is empty." << endl;
 		}
+
 		else {
 			for (int tmp = top; tmp >= 0; tmp--) {
 				cout << stack_array[tmp] << endl;
 			}
 		}
-
-	}
-	int main() 
-	{
-		stackarray s;
-		char ch;
-		while (true) {
-			cout << endl;
-			cout << "\n***stack menu***\n" << endl;
-			cout << "1. push \n" << endl;
-			cout << "2. pop \n" << endl;
-			cout << "3. display \n" << endl;
-			cout << "4. exit \n" << endl;
-			cin >> ch;
-			switch (ch) {
-			case '1': {
-				s.push();
-				break;
-			}
-			case '2':
-				if (s.empty()) {
-					cout << "\nstack is empty." << endl;
-					break;
-				}
-				s.pop();
-				break;
-			case '3':
-				s.display();
-				break;
-
-			case '4':
-			{
-				exit(0);
-			}
-			break;
-
-			default:
-			{
-				cout << "\ninvalid choice" << endl;
-			}
-			break;
-			}
-		}	  
 	}
 };
 
+int main() {
+	stackArray s;
+	char ch;
+
+	while (true)
+	{
+		cout << endl;
+		cout << "\n----Menu Stack----\n" << endl;
+		cout << "1. Push \n" << endl;
+		cout << "2. Pop \n" << endl;
+		cout << "3. Display \n" << endl;
+		cout << "4. Exit \n" << endl;
+		cout << "Masukkan pilihan (1-4): ";
+		cin >> ch;
+		switch (ch)
+		{
+		case '1':
+			s.push();
+			break;
+
+		case '2':
+			if (s.empty()) {
+				cout << "\nStack is empty." << endl;
+				break;
+			}
+			s.pop();
+			break;
+
+		case '3':
+			s.display();
+			break;
+
+		case '4':
+		{
+			exit(0);
+		}
+		break;
+
+		default:
+		{
+			cout << "\nInvalid Choice." << endl;
+		}
+		break;
+		}
+	}
+}
